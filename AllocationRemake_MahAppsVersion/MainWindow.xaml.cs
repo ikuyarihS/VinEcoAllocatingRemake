4e9094c1f007be887c35cd0d54995fb520ee8617
Shirayuki <lethienphuoc1991@gmail.com>
2018-02-02 12:40:00 +0700

    Testing MahApps.Metro
    Testing Stuff.

---
+++ AllocationRemake_MahAppsVersion/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="VinEco">
+//   Shirayuki 2018.
+// </copyright>
+// <summary>
+//   Interaction logic for MainWindow.xaml
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AllocationRemake_MahAppsVersion
+{
+    #region
+
+    using System.Windows;
+
+    using MahApps.Metro.Controls;
+
+    #endregion
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Remake_MahAppsVersion/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Remake_MahAppsVersion
+{
+    #region
+
+    using MahApps.Metro.Controls;
+
+    #endregion
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
